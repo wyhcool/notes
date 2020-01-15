f4937317c7c820e5c7c2635277f0e8e5265809d0
wyhcool <brucewyh2017@gmail.com>
2020-01-15 23:28:39 +0800

    5.7.3
    通过反射来分析类，全面地深入地分析

---
+++ Java/javasrc/corejava10/v1ch05/reflection/ReflectionTest.java
@@ -0,0 +1,119 @@
+import com.sun.tools.internal.jxc.ap.Const;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Scanner;
+
+/**
+ * use reflection to print all features of a class
+ *
+ * @version 1.0.0 2020-01-15
+ * @author bruce
+ */
+public class ReflectionTest {
+
+    public static void main(String[] args) {
+
+        String name;
+        // read class name from command line args or user input
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter class name (e.g. java.util.Date):");
+            name = in.next();
+        }
+
+        try {
+            //print class name and superclass name if (!= Object)
+            Class cl = Class.forName(name);
+            Class supercl = cl.getSuperclass();
+            String modifiers = Modifier.toString(cl.getModifiers());
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+            System.out.print("class " + name);
+            if (supercl != null && supercl != Object.class) {
+                System.out.print(" extends " + supercl.getName());
+            }
+            System.out.print("\n{\n");
+            printConstructors(cl);
+            System.out.println();
+            printMethods(cl);
+            System.out.println();
+            printFields(cl);
+            System.out.println("}");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
+
+    private static void printConstructors(Class cl) {
+        Constructor[] constructors = cl.getDeclaredConstructors();
+        for (Constructor c : constructors) {
+            String name = c.getName();
+            System.out.print("    ");
+            String modifiers = Modifier.toString(c.getModifiers());
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+            System.out.print(name + "(");
+
+            //print parameter type
+            Class[] paramTypes = c.getParameterTypes();
+            for (int i = 0; i < paramTypes.length; i++) {
+                if (i > 0) {
+                    System.out.print(", ");
+                }
+                System.out.print(paramTypes[i].getName());
+            }
+            System.out.println(");");
+        }
+    }
+
+    private static void printMethods(Class cl) {
+        Method[] methods = cl.getDeclaredMethods();
+        for (Method m : methods) {
+            Class retType = m.getReturnType();
+            String name = m.getName();
+
+            System.out.print("    ");
+            String modifiers = Modifier.toString(m.getModifiers());
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+            System.out.print(retType.getName() + " ");
+            System.out.print(name + "(");
+
+            //print parameter type
+            Class[] paramTypes = m.getParameterTypes();
+            for (int i = 0; i < paramTypes.length; i++) {
+                if (i > 0) {
+                    System.out.print(", ");
+                }
+                System.out.print(paramTypes[i].getName());
+            }
+            System.out.println(");");
+        }
+    }
+
+    private static void printFields(Class cl) {
+        Field[] fields = cl.getDeclaredFields();
+        for (Field f : fields) {
+            Class type = f.getType();
+            String name = f.getName();
+
+            System.out.print("    ");
+            String modifiers = Modifier.toString(f.getModifiers());
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+            System.out.print(type.getName() + " ");
+            System.out.print(name);
+            System.out.println(";");
+        }
+    }
+}
